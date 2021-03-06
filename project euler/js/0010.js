feb4a2a3afbeb725e769b64563f80022f724ad00
Ali <aliabbas.299792@gmail.com>
2021-03-06 23:28:57 +0000

    Made it way faster

--- project euler/js/0010.js
+++ project euler/js/0010.js
@@ -1,12 +1,12 @@
 function prime_sum(max){
   const primes = [ 2 ];
   let prime_number = 1; //we start off from the first prime number, 2
-  let last_found_prime = -1;
 
   let current_num = 3;
   while(current_num < max){
     let is_prime = true; //we assume it's a prime
     for(const prime of primes){ //we try to divide by all the prime numbers, one of them must be a factor for it to be a non prime number
+      if(prime**2 > current_num) break; //since to have a large factor you need a small one, so a*b = big_num, max of a or b is when a == b == sqrt(big_num)
       if(!(current_num % prime)){
         is_prime = false;
         break;
@@ -14,7 +14,6 @@ function prime_sum(max){
     }
     if(is_prime){
       prime_number++;
-      last_found_prime = current_num;
       primes.push(current_num);
     }
     current_num++;
