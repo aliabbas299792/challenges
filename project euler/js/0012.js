deb9ed3831f8b0035af0e7c92cf9212c7e404680
Ali <aliabbas.299792@gmail.com>
2021-03-07 14:36:53 +0000

    Done challenge 12

---
+++ project euler/js/0012.js
@@ -0,0 +1,29 @@
+function find_divisors(num){
+  const divisors = [];
+  if(num == 1) divisors.push(1); //add 1 for just this special case
+  let divisor = 1;
+  const original = num;
+  while(divisor**2 < original){ //we will have all divisors above the square root while testing below it (num/divisor could be bigger than the sqrt)
+    if(!(num % divisor)){
+      divisors.push(num/divisor, divisor);
+    }
+    divisor++;
+  }
+  return divisors;
+}
+
+function triangle_num(n){
+  let num = 0;
+  for(let i = 1; i <= n; i++) num += i;
+  return num;
+}
+
+function highly_divisible_triangular_number(){
+  for(let i = 0; i < 15000; i++){
+    let divisors = find_divisors(triangle_num(i)).length;
+    if(divisors >= 500)
+      return triangle_num(i);
+  }
+}
+
+console.log("The first triangle number to have over five hundred divisors is:", highly_divisible_triangular_number());
